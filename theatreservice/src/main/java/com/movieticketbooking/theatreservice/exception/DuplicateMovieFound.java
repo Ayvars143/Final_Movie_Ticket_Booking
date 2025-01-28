7d4c14679c7020b2865dc499306c355486fe195f
Admin <vinay.professionaljavadeveloper@gmail.com>
2025-01-28 08:20:11 -0800

    implemented theater srevices...

--- theatreservice/src/main/java/com/movieticketbooking/theatreservice/exception/DuplicateMovieFound.java
+++
@@ -1,7 +0,0 @@
-package com.movieticketbooking.movieservice.exception;
-
-public class DuplicateMovieFound extends RuntimeException {
-    public DuplicateMovieFound(int id) {
-        super("Duplicate movie found for id: " + id);
-    }
-}
--- theatreservice/src/main/java/com/movieticketbooking/theatreservice/exception/TheaterNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.movieticketbooking.movieservice.exception;
-
-public class TheaterNotFoundException extends RuntimeException {
-    public TheaterNotFoundException(int id) {
-        super("Movie not found with id: " + id);
-    }
-}
